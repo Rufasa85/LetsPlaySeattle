56bfc3700930bb460036ee767a2f1adb3c68cf7e
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-08-22 16:05:29 -0700

    yay?

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-	res.render('index')
+	res.render('index', {key:process.env.GOOGLE_API_KEY,link:process.env.LINK})
 });
 
 app.listen(process.env.PORT || 3000);
