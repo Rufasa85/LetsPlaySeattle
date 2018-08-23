c600fa38926f06a8bb86d22f69f7854d9f0e42aa
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-08-23 15:57:21 -0700

    added a loading spinner

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,9 +1,13 @@
-
+function showPage() {
+ 	document.getElementById("loader").style.display = "none";
+  	document.getElementById("content").style.display = "block";
+}
 function initMap() {
 	navigator.geolocation.getCurrentPosition(position=> {
   		map = new google.maps.Map(document.getElementById('map'), {
     		center: {lat: position.coords.latitude, lng: position.coords.longitude	},
     		zoom: 13
     	})
+    	showPage()
 	})
 }
